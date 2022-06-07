c272350e4fd5dfdb59c9efea5f0740406fe175f3
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-07 00:13:13 -0500

    search by city logic started

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,47 @@
-var apiKey = "8146fc372939ba1529f0cee4a074681a";
-var apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=524901&appid=${apiKey}`;
+// apiKey = "8146fc372939ba1529f0cee4a074681a";
 
-// search button functions
+// search button logic
+function cityLocator() {
+    var city = $("#city-search")[0].value.trim();
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=8146fc372939ba1529f0cee4a074681a"; 
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json()
+            .then(function (data) {
+                $("#city-search")[0].textContent = city + " (" + moment().format('MMM Do YYY, h:mm:ss a') + ")";
+                $("#results").append("cityName");
+
+                var lon = data.coord.lon;
+                var lat = data.coord.lat;
+
+                var location = lat.toString() + " " + lon.toString();
+
+                localStorage.setItem(city, location);
+
+                // update api url to lat/lon position
+                apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=8146fc372939ba1529f0cee4a074681a";
+
+                fetch(apiUrl)
+                .then(function (newResponse) {
+                    if (newResponse.ok) {
+                        newResponse.json()
+                        .then(function (newData) {
+                            currentWeather(newData)
+                        })
+                    }
+                })
+            })
+        } else {
+            alert("Can't find city. Try again!");
+        }
+    })
+}
+
+function currentWeather(data) {
+    
+}
+
 
 // display current and future weather for that city
 
