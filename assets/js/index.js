816176ff09d25ce372fed49b91317d0c51e25130
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-06 22:41:16 -0500

    add pseudocomments and add html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,19 @@
 var apiKey = "8146fc372939ba1529f0cee4a074681a";
 var apiUrl = `https://api.openweathermap.org/data/2.5/weather?id=524901&appid=${apiKey}`;
+
+// search button functions
+
+// display current and future weather for that city
+
+// save search history to localstorage
+
+// display city name, date, and icon to represent: weather, 
+// temp, humidity, wind speed, and uv index
+
+// view uv index
+// color to indicate whether the conditions are: favorable, moderate, or severe
+
+// view future conditions: 5 day forecast that displays:
+// date, icon to represent weather, temp, wind speed, and humidity 
+
+// display current and future conditions for that city again 
