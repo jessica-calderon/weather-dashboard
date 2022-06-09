331f26f9a71f9776f17c284834daa148b603ef8a
jessica-calderon <calderonjessica13@yahoo.com>
2022-06-09 02:27:10 -0500

    mvp functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,6 +41,7 @@ function search(city) {
         var newDivEl = $('<div>')
         newDivEl.append(dateEl, icon, temp, humidity, wind);
         $("#forecast").html(newDivEl);
+
 // lat/lon for searched city //
         var lat = response.coord.lat;
         var lon = response.coord.lon;
@@ -57,9 +58,61 @@ function search(city) {
             $('#uv-levels').html(uvDiv);
         });
     });
-}
 
 /* weekly forecast logic */
+$.ajax({
+    url: weatherUrl,
+    method: 'GET'
+})
+.then(function (response) {
+    // store forecast to an array list //
+    var returnForecast = response.list;
+    $("#weekly").empty();
+    for (var i = 0; i < returnForecast.length; i += 7) {
+        // forecast div element
+        var forecastDiv = $("<div class='div-style card text-white bg-primary mx-auto mb-10 p-2 shadow-lg'>");
+        // forecast temp, date, and humidty variables
+        var forecastDate = returnForecast[i].dt_txt;
+        var setFcDate = forecastDate.substr(0,10);
+        var mainTemp = returnForecast[i].main.temp;
+        var hum = returnForecast[i].main.humidity;
+
+
+        // forecast results html elements 
+        var indexDate = $("<h5 class='card-title'>").text(setFcDate);
+        var tempEl = $("<p class='card-text'>").text("Temperature: " + mainTemp);
+        var humEl = $("<p class='card-text'>").text("Humidity: " + hum); 
+
+        var forecastWeather = returnForecast[i].weather[0].main 
+        // forecast weather icons 
+        if (forecastWeather === "Rain") {
+            var forecastIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/09d.png");
+            forecastIcon.attr("style", "width: 40px; height: 40px");
+        } else if (forecastWeather === "Drizzle") {
+            var forecastIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/10d.png");
+            forecastIcon.attr("style", "width: 40px; height: 40px");
+        } else if (forecastWeather === "Clouds") {
+            var forecastIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/03d.png");
+            forecastIcon.attr("style", "width: 40px; height: 40px");
+        } else if (forecastWeather === "Clear") {
+            var forecastIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
+            forecastIcon.attr("style", "width: 40px; height: 40px");
+        } else if (forecastWeather === "Snow") {
+            var forecastIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/13d.png");
+            forecastIcon.attr("style", "width: 40px; height: 40px");
+        }
+
+        // append forecast to index
+        forecastDiv.append(indexDate);
+        forecastDiv.append(forecastIcon);
+        forecastDiv.append(tempEl);
+        forecastDiv.append(humEl);
+        $("#weekly").append(forecastDiv);
+        }
+    });
+
+}
+loadSaved();
 
 /* city location logic */
 $("#search-btn").on("click", function(event) {
@@ -74,9 +127,10 @@ $("#search-btn").on("click", function(event) {
     search(citySearch);
     loadSaved();
 });
+
 /* load localStorage and display on page */
 function loadSaved () {
-    var searchedItem = json.parse(localStorage.getItem('city'));
+    var searchedItem = JSON.parse(localStorage.getItem('city'));
     var savedSection = $("<button class='styled-btn btn border text-muted mt-1 shadow-sm bg-white rounded'>").text(searchedItem);
     var divEl = $("<div>");
     divEl.append(savedSection)
